4fbe41d58be5e815f63193b4192f27628084ba44
shengxi <404833948@qq.com>
2019-09-04 00:32:53 +0800

    [feat]调整哎呦精英跳跃高度

--- 一键lua脚本-精英哎呦城.lua
+++ 一键lua脚本-精英哎呦城.lua
@@ -14,6 +14,8 @@ common.simpleStart({
 		tobot_fastladder = 0, -- 取消快速爬梯，防止大桥下楼梯不稳定
 	},
 	minXSpeed = 850,  -- 站街速低于850会死掉
+	minYSpeed = 1150,
+	maxYSpeed = 2000,
 
     -- 脚本名称列表，需和mapIds一一对应
     -- 支持16进制hex编码脚本

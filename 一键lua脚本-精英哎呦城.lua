c223d08fe20abaacfa0cb153a393fa68d71f1ca5
Jflamoli <597673687@qq.com>
2019-09-04 21:58:58 +0800

    111

--- 一键lua脚本-精英哎呦城.lua
+++ 一键lua脚本-精英哎呦城.lua
@@ -14,7 +14,7 @@ common.simpleStart({
 		tobot_fastladder = 0, -- 取消快速爬梯，防止大桥下楼梯不稳定
 	},
 	minXSpeed = 850,  -- 站街速低于850会死掉
-	minYSpeed = 1150,
+	minYSpeed = 1120,
 	maxYSpeed = 2000,
 
     -- 脚本名称列表，需和mapIds一一对应

2cde29a4a4b39594fd6ce3f00e4e3fc1461106a1
shengxi <404833948@qq.com>
2019-10-07 21:05:22 +0800

    [feat]图书馆、神殿优化完成

--- 一键lua脚本-1(中分辨率)神殿活动.lua
+++ 一键lua脚本-1(中分辨率)神殿活动.lua
@@ -35,11 +35,13 @@ common.shendianStart({
         taskIds = {43603201, 43603202, 43603203, 43603204, 43603205, 43603206},
     },
     onScriptRound = function  ()
-        if (getmapid() == 68202 or getmapid() == 68203) then
-            sleep(3000)
-            if (common.checkMob('终极神殿二阶 英雄王')) then
-                speak('遇到英雄王了')
-                useskill(43501018,1) -- 使用猴子无敌
+        if (getmapid() == 68202) then
+            while (true) do
+                if (common.checkMob('终极神殿二阶 英雄王')) then
+                    speak('遇到英雄王了')
+                    useskill(43501018,1) -- 使用猴子无敌
+                    break
+                end
             end
         end
     end,

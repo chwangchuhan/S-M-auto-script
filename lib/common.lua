1ef46f1b17abfdafc07e253f2ae64cb4c29fda85
Jflamoli <597673687@qq.com>
2019-11-07 18:39:05 +0800

    1111

--- lib/common.lua
+++ lib/common.lua
@@ -22,8 +22,8 @@ local suduConfig2 = loadfile(path_scripts.."S-M-auto-script\\config\\ËÙ¶È×°.lua"
 
 SMCode = '35f03a61-31f7-4c1a-b18c-5f4e9f6a5aa1'
 
-local defaultXMinSpeed = 1000
-local defaultXMaxSpeed = 1480
+local defaultXMinSpeed = 500
+local defaultXMaxSpeed = 1950
 local defaultYMinSpeed = 1050
 local defaultYMaxSpeed = 1450
 
